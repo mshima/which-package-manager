da51efda04be36ee706730c472abba79f15417d1
Marcelo Shima <marceloshima@gmail.com>
2025-03-24 17:05:50 -0300

    chore: update devDependencies (#6)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/naming-convention, n/file-extension-in-import */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
